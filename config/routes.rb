62982040e2dd185214e8e4241ea9a2b15bb402ed
henryboswell <Franklin.henry.boswell@gmail.com>
2016-12-28 17:12:21 -0500

    generate welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   devise_for :users
   resources :groups do
   	resources :individuals
   end
+
+
+  authenticated :user do 
+  	root "groups#index", as: "authenticated_root"
+  end
+
+
+
+  root'welcome#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
