e7bd9db4013c30b07e5c449bebd062a2c976c4ae
henryboswell <Franklin.henry.boswell@gmail.com>
2016-12-28 16:54:01 -0500

    Assosiate user and groups

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161228214450) do
+ActiveRecord::Schema.define(version: 20161228214803) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "individuals", force: :cascade do |t|
