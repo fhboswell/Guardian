c09e2c99b45cab493afbc22fdfb8bfc7f1cc7bde
henryboswell <Franklin.henry.boswell@gmail.com>
2017-01-08 00:20:15 -0800

    fix api

--- app/controllers/api/v1/groupsapi_controller.rb
+++ app/controllers/api/v1/groupsapi_controller.rb
@@ -2,13 +2,14 @@ module Api
   module V1
 
 	  class GroupsapiController < AuthenticationController
-	 	
+	 	before_action :authenticate_request!
 		def show
 			
-	  	item =  Group.where(user_id: current_user)
+	  		item =  Group.where(user_id: current_user)
 		
-		render :json => item.to_json
+			render :json => item.to_json
 		end 
+
 
 
 	  end
