4db2af6889a78b3c7fafd4342f5871750c8dff06
fhboswell <franklin.henry.boswell@gmail.com>
2017-04-16 21:53:59 -0700

    API Individual

--- app/controllers/api/v1/individualsapi_controller.rb
+++ app/controllers/api/v1/individualsapi_controller.rb
@@ -4,7 +4,7 @@ module Api
 	  class IndividualsapiController < AuthenticationController
 	 	before_filter :authenticate_request!
 	 	before_action :set_group
-		before_action :set_individual, except: [:create]
+		
 
 		def index
 
