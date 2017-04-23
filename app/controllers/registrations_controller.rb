147af5cc431523fc67a1148d0bfd8b90fcc17c37
fhboswell <franklin.henry.boswell@gmail.com>
2017-04-23 16:40:27 -0700

    added status codes to signup api endpoint

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -10,8 +10,8 @@ class RegistrationsController < Devise::RegistrationsController
       }
        format.json {
         @user = User.create(user_params)
-        @user.save ? (render :json => {:state => {:code => 0}, :data => @user }) : 
-                     (render :json => {:state => {:code => 1, :messages => @user.errors.full_messages} })
+        @user.save ? (render json: {:data => @user }, status: :created) : 
+                     (render json: {:data => @user.errors.full_messages }, status: :conflict)
       }
     end
   end
