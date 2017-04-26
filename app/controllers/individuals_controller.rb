cc078388e324ac23e4cad27cf0a328e38b73339b
fhboswell <franklin.henry.boswell@gmail.com>
2017-04-25 21:44:53 -0700

    removed a local test

--- app/controllers/individuals_controller.rb
+++ app/controllers/individuals_controller.rb
@@ -6,8 +6,7 @@ class IndividualsController < ApplicationController
 	def create
 		
 		@individual = @group.individuals.create(individual_params)
-		@user = User.new(:email => '1@example.com', :password => 'password', :type_key => '1')
-        @user.save
+		
 
 		redirect_to @group
 	end
