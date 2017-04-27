d89252c2af00cf0b5ace5a118733d362557bc53c
fhboswell <franklin.henry.boswell@gmail.com>
2017-04-26 18:34:24 -0700

    hide the sendgridkey

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
   :user_name => 'apikey',
-  :password => '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',
+  :password => ENV['Sendgrid_Key'],
   :domain => 'yourdomain.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
