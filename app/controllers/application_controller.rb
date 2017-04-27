1e633a8ed4c2166a38a32db1ac417e99c9d1f327
fhboswell <franklin.henry.boswell@gmail.com>
2017-04-27 16:04:39 -0700

    fixed travis things

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   
 
-  before_filter :configure_devise_params, if: :devise_controller?
+  before_action :configure_devise_params, if: :devise_controller?
   def configure_devise_params
 
     devise_parameter_sanitizer.permit(:sign_up) do |u|
