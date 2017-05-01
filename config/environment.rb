82ffb12c04ddfdae9eca5c57cddc9f2d64b54119
fhboswell <franklin.henry.boswell@gmail.com>
2017-05-01 12:06:41 -0700

    fixed env var setup

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,8 @@
 require_relative 'application'
 
 # Initialize the Rails application.
+#app_environment_variables = File.join(Rails.root, 'config', 'local_env.yml')
+#load(app_environment_variables) if File.exists?(app_environment_variables)
 Rails.application.initialize!
 
 
